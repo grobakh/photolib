9e40614782f75c9be7e4abd89d83f8bcee7fd366
Vlad Dupelev <vladimir.dupelev@gmail.com>
2015-04-04 03:18:44 +0300

    port 8080

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ function normalizePort(val) {
     return false;
 }
 
-var port = normalizePort(process.env.PORT || '3000');
+var port = normalizePort(process.env.PORT || '8080');
 app.set('port', port);
 
 var server = http.createServer(app);

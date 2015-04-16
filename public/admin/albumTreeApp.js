cffca6f9659ba5e6071ccfda98d8adb20433b8fc
Vlad Dupelev <vladimir.dupelev@gmail.com>
2015-04-17 01:53:18 +0300

    remove, moveUp, moveDown

--- public/admin/albumTreeApp.js
+++ public/admin/albumTreeApp.js
@@ -3,7 +3,24 @@
 
   albumTreeApp.controller('treeController', ["$scope", "$http",
     function ($scope, $http) {
-      function findParent() {
+      function findParentScope(node, scope) {
+        if (!scope) {
+          return null;
+        }
+
+        var position = scope.indexOf(node);
+
+        if (position > -1) {
+          return scope;
+        }
+
+        for (var i = scope.length; i--;) {
+          var candidate = findParentScope(node, scope[i].children);
+          if (candidate != null) {
+            return candidate;
+          }
+        }
+
         return null;
       }
 
@@ -42,16 +59,64 @@
       $scope.remove = function () {
         if ($scope.albums.currentNode) {
           var node = $scope.albums.currentNode;
-          var parent = findParent($scope.albums.currentNode, $scope.albumTree);
-          if (parent) {
-            parent.children.splice(parent.children.indexOf(node), 1);
+          var parentScope = findParentScope($scope.albums.currentNode, $scope.albumTree);
+
+          if (parentScope) {
+            parentScope.splice(parentScope.indexOf(node), 1);
             $scope.albums.currentNode = undefined;
           }
         }
       };
 
+      $scope.moveUp = function () {
+        if ($scope.albums.currentNode) {
+          var node = $scope.albums.currentNode;
+          var parentScope = findParentScope($scope.albums.currentNode, $scope.albumTree);
+
+          if (parentScope) {
+            var oldIndex = parentScope.indexOf(node);
+            var newIndex = oldIndex > 0 ? oldIndex - 1 : oldIndex;
+            parentScope.splice(oldIndex, 1);
+            parentScope.splice(newIndex, 0, node);
+          }
+        }
+      };
+
+      $scope.moveDown = function () {
+        if ($scope.albums.currentNode) {
+          var node = $scope.albums.currentNode;
+          var parentScope = findParentScope($scope.albums.currentNode, $scope.albumTree);
+
+          if (parentScope) {
+            var oldIndex = parentScope.indexOf(node);
+            var newIndex = oldIndex < parentScope.length - 1 ? oldIndex + 1 : oldIndex;
+            parentScope.splice(oldIndex, 1);
+            parentScope.splice(newIndex, 0, node);
+          }
+        }
+      };
+
+      function purify(scope) {
+        if (!scope) {
+          return;
+        }
+
+        for (var i = scope.length; i--;) {
+          var item = scope[i];
+          delete item.selected;
+          delete item.collapsed;
+
+          if (item.children) {
+            purify(item.children);
+          }
+        }
+      }
+
       $scope.saveChanges = function () {
-        $http.post('/admin/manageAlbums/saveChanges', {albumTree: $scope.albumTree}).
+        data = angular.copy($scope.albumTree);
+        purify(data);
+
+        $http.post('/admin/manageAlbums/saveChanges', {albumTree: data}).
           success(function (data) {
             var paramStr = _.getURIparams(data);
             window.location = ("/admin/manageAlbums/success?" + paramStr);
